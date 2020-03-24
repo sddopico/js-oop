2022a14925fd0406c8764f0ed90a4381eee4a5ca
Dominic Dopico <serafin.dopico@gmail.com>
2020-03-23 18:40:49 -0700

    factory functions lecture and demo

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 
-const circle = {
-  radius: 1,
-  location: {
-    x: 1,
-    y: 2
-  },
-  draw: function() {
-    console.log('draw');
-  }
+function createCircle (radius) {
+  return {
+    radius,
+    draw: function() {
+      console.log('draw');
+    }
+  };
 }
 
+const circle = createCircle(1);
 circle.draw();
