25396bfe82127d74650f20f9365edbffe44b2c0e
Dominic Dopico <serafin.dopico@gmail.com>
2020-03-23 19:14:36 -0700

    constructor props

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 
-function createCircle (radius) {
-  return {
-    radius,
-    draw: function() {
-      console.log('draw');
-    }
-  };
-}
 
-const circle = createCircle(1);
-circle.draw();
+//Constructor function
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function() {
+    console.log('draw');
+  }
+}
+const another = new Circle(1);
